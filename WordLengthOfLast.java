45422eb5db937d6a2a0ae1c775b12ffbb0165c21
Andy Zhao <294189227@qq.com>
2017-01-26 11:18:27 +0800

    创建WordLengthOfLast.java
    计算字符串最后一个单词的长度

---
+++ WordLengthOfLast.java
@@ -0,0 +1,34 @@
+package string;
+
+import java.util.Scanner;
+
+/**
+ * 字符串最后一个单词的长度
+ * @author Administrator
+ *
+ */
+public class WordLengthOfLast {
+
+	public static void main(String[] args) {
+		System.out.println("请输入一个字符串,每个单词中间用空格隔开: ");
+		Scanner input = new Scanner(System.in);
+		while(input.hasNextLine()){
+			String str = input.nextLine();
+			int length = getLength(str);
+			System.out.println("字符串最后一个单词的长度为: " + length);
+		}
+	}
+	
+	public static int getLength(String str) {
+		int length = 0;
+		
+		if(str != null && str.length() > 1) {
+			String[] arr = str.split(" ");
+			length = arr[arr.length - 1].length();
+		}else{
+			length = 0;
+		}
+		
+		return length;
+	}
+}
